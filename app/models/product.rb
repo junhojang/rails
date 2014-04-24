0339c2de42cd3e7d97613828398691f8e51d219b
junho jang <jangjunho7@gmail.com>
2014-04-24 00:03:11 -0700

    validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ActiveRecord::Base
+  validates :title,  :desc,  :img_url,  presence: true
+  validates_numericality_of :price, :greater_than_or_equal_to => 1
+  validates :title,  uniqueness: true
+  validates :img_url,  allow_blank: true,  format: {
+  	with: %r{\.(gif|jpg|png)\Z}i,
+  	message: 'must be a URL for GIF, JPG, PNG image'
+  }
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,48 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :products
+
+  def create_test_product(_img_url)
+    return Product.new(title:'title',desc:'desc',img_url:_img_url,price:100)
+  end
+
+  test "product attrs must not be empty" do
+  	product=Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:desc].any?
+    assert product.errors[:price].any?
+    assert product.errors[:img_url].any?
+  end
+
+  test "product price must be positive" do
+    product=Product.new(title:'title',desc:'desc',img_url:'ruby.jpg')
+    product.price=1
+    assert product.valid?
+
+    product.price=0
+    assert product.invalid?
+
+    product.price=-1
+    assert product.invalid?
+  end
+
+  test "product image must be jpg || png || gif" do
+    good_case=%w{test.jpg test.JPG test.png test.PNG test.gif test.GIF}
+    bad_case=%w{test.aaa test.doc test.hwp}
+
+    good_case.each do |c|
+      assert create_test_product(c).valid?
+  	end
+
+  	bad_case.each do |c|
+      assert create_test_product(c).invalid?
+  	end
+  end
+
+  test "product title must be unique" do
+    product=Product.new(title:products(:one).title,desc:'desc',price:100,img_url:'ruby.jpg')
+    assert product.invalid?
+  end
 end
