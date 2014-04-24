c5fdb1dad352730a5fe9c5c26b3f14e2299fdd70
junho jang <jangjunho7@gmail.com>
2014-04-23 20:51:10 -0700

    shop scaffold

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.create(
+	title:'ruby',
+	desc:%{<p>this is ruby book</p>},
+	img_url:'ruby.jpg',
+	price:49.95
+	)
